3a41d5b5b2143ac5bb9e02d0b08f646d81217ab6
sylvain099 <sylvain099@hotmail.fr>
2016-09-19 13:18:42 +0200

    toutSousSequence

---
+++ header/touteSousSequence.h
@@ -0,0 +1,13 @@
+#ifndef __TOUTESOUSSEQUENCE_H__
+#define __TOUTESOUSSEQUENCE_H__
+
+/**
+ * \fn int * generateTab( int size, bool init )
+ * \author Sylvain De Barros, Yohan Roux 
+ * \brief cherche les sous séquence d'un tableau en comparant toute les sous séquence 
+ * \param[in] t tableau contenant les séquence 
+ * \param[in] n taille du tableau  
+ */
+touteSousSequence(int t[], int n);
+
+#endif 
