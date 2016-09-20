21454d94340f1f0b29010f7998b45a33b65ff4d3
sylvain099 <sylvain099@hotmail.fr>
2016-09-20 13:35:16 +0200

    pseudo code algo3....

--- src/algo2.c
+++ src/algo2.c
@@ -2,27 +2,27 @@
 #include "../header/algo2.h"
 
 Result aglo2(int t[], int debut, int fin){
-    int max = t[0],
+  int max = t[0],
   indiceD = debut,
   indiceF = debut,
   indiceCourrantD = debut,
   indiceCourrantF = debut;
 
   while (indiceCourrantD < fin ){
-      int tmp = 0;
-      while (indiceCourrantF < fin){
-       int maxTmp = tmp + t[indiceCourrantF];
-       if (maxTmp > max){
-        max = maxTmp;
-        indiceD = indiceCourrantD;
-        indiceF = indiceCourrantF;
+    int tmp = 0;
+    while (indiceCourrantF < fin){
+     int maxTmp = tmp + t[indiceCourrantF];
+     if (maxTmp > max){
+      max = maxTmp;
+      indiceD = indiceCourrantD;
+      indiceF = indiceCourrantF;
     }
     tmp = maxTmp;
     indiceCourrantF++;
-}
+  }
 
-indiceCourrantF = indiceCourrantD +1;
-indiceCourrantD++;
+  indiceCourrantF = indiceCourrantD +1;
+  indiceCourrantD++;
 }
 Result resultat; 
 resultat.debut = indiceD;
